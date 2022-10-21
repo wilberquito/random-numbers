620531743f1ae2cd0e0d4090cd0d2d9c6249f6a2
wilberquito <wbq.software@gmail.com>
2022-10-22 00:38:26 +0200

    randoms generator example

--- rgen.py
+++ rgen.py
@@ -1,13 +1,20 @@
 import random
 from random import seed
+import pandas as pd
 
 seed(1)
 
+export_path = 'randoms.csv'
 min = 1979
 max = 2561
 n = max - min
+values = []
+
 
 for _ in range(n):
     r = random.randint(min, max) 
-    print(r)
+    values.append(r)
+
+df = pd.DataFrame(values, columns=['random'])
+df.to_csv(export_path)
 
